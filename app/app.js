2e463f06fce089555b88adf6572873d47a7f5642
sakis <destrotns@gmail.com>
2018-05-15 16:17:05 +0300

    Initial Commit,design only

---
+++ app/app.js
@@ -0,0 +1,9 @@
+console.log(twitch);
+
+const app = angular.module('mainApp', []);
+
+let mainController = function($scope, $filter){
+
+}
+
+app.controller('mainController', ['$scope', '$filter', mainController]);
